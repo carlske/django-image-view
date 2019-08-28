926f838ffbbfc173c561877c98f1a6af731dff98
CarlsKe <fernancarldiaz@outlook.com>
2019-08-28 12:28:09 -0500

    style: changed size and order widget

--- image_view/widgets.py
+++ image_view/widgets.py
@@ -3,40 +3,18 @@ from django.utils.safestring import mark_safe
 from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
 
-class ImageView(widgets.Widget):
-    """ Image View widget """
-    
-    template_name = 'django-image-view.html'
+
+FILE_INPUT_CONTRADICTION = object()
+
+
+class ImageViewWidget(widgets.FileInput):
     clear_checkbox_label = _('Clear')
     initial_text = _('Currently')
     input_text = _('Change')
-    type_input = 'file'
-
-    def get_context(self, name, value, attrs):
-        context = super().get_context(name, value, attrs)
-        checkbox_name = self.clear_checkbox_name(name)
-        checkbox_id = self.clear_checkbox_id(checkbox_name)
-        context['widget'].update({
-            "type": self.type_input,
-            'url': self.get_url_image(value),
-            'checkbox_name': checkbox_name,
-            'checkbox_id': checkbox_id,
-            'is_initial': self.is_initial(value),
-            'input_text': self.input_text,
-            'initial_text': self.initial_text,
-            'clear_checkbox_label': self.clear_checkbox_label,
-        })
-        return context
+#    template_name = 'django/forms/widgets/clearable_file_input.html'
+    template_name = 'django-image-view.html'
 
 
-    def get_url_image(self,value):
-        """
-        Given the url of the file input, return the url image to show image.
-        """
-        if hasattr(value,'url'):
-            return value.url
-        return ''
-    
     def clear_checkbox_name(self, name):
         """
         Given the name of the file input, return the name of the clear checkbox
@@ -55,3 +33,47 @@ class ImageView(widgets.Widget):
         Return whether value is considered to be initial value.
         """
         return bool(value and getattr(value, 'url', False))
+
+    def format_value(self, value):
+        """
+        Return the file object if it has a defined url attribute.
+        """
+        if self.is_initial(value):
+            return value
+
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        checkbox_name = self.clear_checkbox_name(name)
+        checkbox_id = self.clear_checkbox_id(checkbox_name)
+        context['widget'].update({
+            'checkbox_name': checkbox_name,
+            'checkbox_id': checkbox_id,
+            'is_initial': self.is_initial(value),
+            'input_text': self.input_text,
+            'initial_text': self.initial_text,
+            'clear_checkbox_label': self.clear_checkbox_label,
+        })
+        return context
+
+    def value_from_datadict(self, data, files, name):
+        upload = super().value_from_datadict(data, files, name)
+        if not self.is_required and widgets.CheckboxInput().value_from_datadict(
+                data, files, self.clear_checkbox_name(name)):
+
+            if upload:
+                # If the user contradicts themselves (uploads a new file AND
+                # checks the "clear" checkbox), we return a unique marker
+                # object that FileField will turn into a ValidationError.
+                return FILE_INPUT_CONTRADICTION
+            # False signals to clear any existing value, as opposed to just None
+            return False
+        return upload
+
+    def use_required_attribute(self, initial):
+        return super().use_required_attribute(initial) and not initial
+
+    def value_omitted_from_data(self, data, files, name):
+        return (
+            super().value_omitted_from_data(data, files, name) and
+            self.clear_checkbox_name(name) not in data
+        )
